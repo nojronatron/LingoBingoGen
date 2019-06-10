fc8ea3c4322bb3755b250c45dd640ea693a8ee0e
Jon Rumsey <jon.rumsey@gmail.com>
2019-06-10 15:59:00 -0700

    Configured UnitTest Project with NUnit and created placeholder unit tests.

---
+++ LingoBingoGenerator.Tests/LingoBingoUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using BingoBoard;
+
+namespace LingoBingoGenerator.Tests
+{
+    [TestFixture]
+    public class LingoBingoUnitTests
+    {
+        Bingoboard _bingoBoard = new Bingoboard
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NUnitTestProject_LingoBingo/UnitTest1.cs
@@ -0,0 +1,64 @@
+using NUnit.Framework;
+using System;
+
+namespace LingoBingoTests
+{
+    public class NUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            string[] strArray = {"method", "inheritance", "class", "object", "instance", "property", "field", "constructor",
+                                    "dot net", "array", "main", "curly brace", "string", "boolean", "semicolon", "parse",
+                                    "try catch", "partial", "return", "call", "override", "keyword", "get or set", "static"};
+            LingoBingoGenerator.BingoBoard _bb = new LingoBingoGenerator.BingoBoard(strArray);
+        }
+        [Test]
+        public void Test_CTOR()
+        {
+            try
+            {
+                LingoBingoGenerator.BingoBoard _bb2 = new LingoBingoGenerator.BingoBoard();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("", ex.Message);
+            }
+        }
+        [Test]
+        public void Test_longestWord()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_longestWordLen()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_TileWidth()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_HorizFrameLength()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_SetFreeSpace()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_GetBoard()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_Randomizer()
+        {
+            Assert.Fail();
+        }
+    }
+}
