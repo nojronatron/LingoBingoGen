768a8777e36f7b1b1ee0e960e722b9b662eb4f03
Jon Rumsey <jon.rumsey@gmail.com>
2019-09-09 20:23:09 -0700

    Added Entity Framework CodeFirst and XML Linq to read/store data via XML to/from SQL DB.

---
+++ LingoBingoGen/LingoDbInitializer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace LingoBingoGen
+{
+    public class LingoDbInitializer : DropCreateDatabaseAlways<LingoContext>
+                                   // DropCreateDatabaseIfModelChanges<LingoContext>
+    {
+        public override void InitializeDatabase(LingoContext context)
+        {   // from https://stackoverflow.com/questions/7004701/cannot-drop-database-because-it-is-currently-in-use-how-to-fix
+            context.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction
+            , string.Format("ALTER DATABASE [{0}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE", 
+                             context.Database.Connection.Database));
+            base.InitializeDatabase(context);
+        }
+        protected override void Seed(LingoContext context)
+        {
+            Console.WriteLine("***** Executing Initializer custom Seed() method. *****");
+            List<LingoWord> lingoWords = GetDataFromXDocument();
+
+            using (LingoContext db = new LingoContext())
+            {
+                // generate entities via the LingoWord class
+                foreach (LingoWord wrd in lingoWords)
+                {
+                    db.LingoWords.Add(wrd);
+                }
+                int rowsSaved = db.SaveChanges();
+                base.Seed(context);
+                if (rowsSaved > 0)
+                {
+                    Console.WriteLine($"LingoWord Entities created. {rowsSaved} rows saved.");
+                }
+                else
+                {
+                    Console.WriteLine("LingoWord Entity creation failed. Press <Enter> to Continue. . .");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine("End of Initialization and Seed code.");
+            Console.WriteLine("Review SQL Server database 'LingoDb', table 'LingoWords'.");
+        }
+        private List<LingoWord> GetDataFromXDocument()
+        {
+            // Load data from an XML file
+            // TODO: Test and debug this for non-existant file; bad data format; whatever else
+            XDocument xWords = XDocument.Load("initialLingoWords.xml");
+
+            // gather 'Item' elements from xDocument into an iterable collection
+            IEnumerable<XElement> itemsXml = xWords.Descendants("Item");
+
+            // Create a generic list of Words from XML elements in the iterable itemsXml
+            List<LingoWord> listWord = new List<LingoWord>(itemsXml.Count());
+            LingoWord item = null;
+            foreach (XElement xItem in itemsXml)
+            {
+                item = new LingoWord()
+                {
+                    Category = xItem.Element("Category").Value,
+                    Word = xItem.Element("Word").Value
+                };
+                listWord.Add(item);
+            }
+            return listWord;
+        }
+    }
+}
---
+++ LingoBingoGen/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace LingoBingoGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!CallInitializerRoutine())
+            {
+                Console.Write("\n\nUnable to load initializer file, an error occurred.");
+                Console.ReadLine();
+                return;     // force program execution exit
+            }
+
+            // set the Console Window size
+            Console.SetWindowSize(150, 40);
+
+            // Query the DB to get the terms and shove them into a List
+            using (LingoContext context = new LingoContext())
+            {
+                List<LingoWord> lingoList = (from w in context.LingoWords
+                                             select w).ToList();
+
+                // get the list of Categories and present the to the user to pick one
+
+                // try it using a few Linq queries and a helper method (seems difficult)
+                string[] allCategories = (from x in context.LingoWords
+                                          select x.Category).ToArray();
+                string userCategory = GetCategorySelection(allCategories);
+                //string[] selectedCategoryWords = (from y in context.LingoWords
+                                                  //where IsCategory(y, userCategory)
+                                                  //select y.Word).ToArray();
+
+                // try it using String.Contains() built-in method then manually iterate to select each Word
+                string[] selectedLingoWords = (from z in context.LingoWords
+                                where z.Category.Contains(userCategory)
+                                select z.Word).ToArray();
+
+                // Instantiate BingoBoard using input object of expected Type
+                BingoBoard alpha = new BingoBoard(selectedLingoWords);
+
+                // send the array to a static function that will "draw" the board
+                Console.WriteLine(alpha.GetBoard());
+            }
+
+
+            Console.Write("\n\nPress <Enter> to Exit. . .");
+            Console.ReadLine();
+        }
+        private static bool CallInitializerRoutine()
+        {
+            bool result = false;
+            try
+            {   // query the database
+                using (LingoContext dbContext = new LingoContext())
+                {
+                    List<LingoWord> lingoWords = (from lw
+                                                  in dbContext.LingoWords
+                                                  select lw).ToList();
+                    if (dbContext.LingoWords.Count() > 0)
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Linq results returned. Initializer probably didn't run.");
+                        result = false; // just being explicit here
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                result = false;         // just being explicit here
+            }
+            return result;
+        }
+        private static string GetCategorySelection(string[] categories)
+        {   // TODO: Write the menu system code that returns an appropriate (strong)selection
+            Console.WriteLine("This is the menu system.");
+            return categories[0];
+        }
+        public static bool IsCategory(LingoWord w, string c)
+        {
+            if (w.Category == c)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
