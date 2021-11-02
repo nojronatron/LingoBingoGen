5aadb4244d4a478539c72a30668bc31f96b7e0cb
Jonathan Rumsey <jonathan.rumsey@bellevuecollege.edu>
2021-11-01 17:17:39 -0700

    Checkpoint.

---
+++ LingoBingoWASM/Library/BingoPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace LingoBingoWASM.Library
+{
+    public class BingoPlayer
+    {
+        [Required]
+        [StringLength(40, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+    }
+}
---
+++ LingoBingoWASM/Library/BingoTileModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LingoBingoWASM.Library
+{
+    public class BingoTileModel
+    {
+        public string Word { get; set; }
+        public bool IsDaubered { get; set; } = false;
+        public BingoTileModel() {}
+        public BingoTileModel(string word)
+        {
+            Word = word;
+        }
+    }
+}
